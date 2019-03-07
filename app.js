16a225ad08331b7333712e41e2a17a324fe8bc12
pattasing <pattarawadee-s@hotmail.com>
2019-03-07 19:06:13 +0700

    add phDetail.ejs, header.ejs, footer.ejs in folder template

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -12,6 +11,16 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/phDetail/:phValue', function(req, res) {
+  var phValue = req.params.phValue;
+  //res.render('phDetail');
+  res.render('phDetail', {phValue: phValue});
+});
+
+app.get('/phDetail', function(req, res) {
+  res.redirect("/phDetail");
 });
 
 app.listen(3000, function() {
