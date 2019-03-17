2296e005b5bee001b0f8527d3a4035b40538e48f
mild supanat <mildsupanat@hotmail.com>
2019-03-17 17:06:13 +0700

    init gamepad control

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+var gamepad = require("gamepad");
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -49,6 +50,51 @@ app.post('/addPond', function(req, res) {
   }
 });
 
+app.get('/control', function(req, res) {
+  var status = gamepadCotrol();
+  scanGamepad();
+  res.render('control', {status: status});
+});
+
 app.listen(3000, function() {
   console.log("Server are running on port 3000");
 });
+
+function gamepadCotrol() {
+  gamepad.init();
+  if (gamepad.numDevices() == "1") {
+    status = "connected"
+    return status;
+  }
+  else {
+    return "Not connected"
+  }
+}
+
+function scanGamepad() {
+  setInterval(gamepad.processEvents, 16);
+  setInterval(gamepad.detectDevices, 500);
+  gamepad.on("up", function(id, num) {
+    if (num == 5 || num == 6 || num == 7 || num == 8) {
+        console.log("Stop");
+    }
+  });
+  gamepad.on("down", function(id, num) {
+    switch (num) {
+      case 5:
+        console.log("forward");
+        break;
+      case 6:
+        console.log("backward");
+        break;
+      case 7:
+        console.log("turn left");
+        break;
+      case 8:
+        console.log("turn right");
+        break;
+      default:
+      console.log("Can't use this button");
+    }
+  });
+}
